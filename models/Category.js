0ecebf747f07b683c42ba926b3bca4e65269f654
Lanay Brown <lanayb.03@gmail.com>
2022-02-11 19:59:38 -0500

    made database, made database models

--- models/Category.js
+++ models/Category.js
@@ -6,6 +6,16 @@ class Category extends Model {}
 
 Category.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      // referemces the category model's id
+    }
     // define columns
   },
   {
