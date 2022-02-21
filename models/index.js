189ab699a1e100dcf83c7fcdd29eb2e90f961a86
Lanay Brown <lanayb.03@gmail.com>
2022-02-21 17:12:18 -0500

    update

--- models/index.js
+++ models/index.js
@@ -6,8 +6,7 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
    Product.belongsTo(Category, {
-    foreignKey: 'category_id',
-    onDelete: 'CASCADE'
+    foreignKey: 'category_id'
 
    });
 // Categories have many Products
